998e6d609d5102921779adf57cbbcaa85dc23a32
glavigna <gonzalolavigna@gmail.com>
2018-03-27 19:19:00 -0300

    Se realiza la primera iteracion de la maquina de estado correspondiente a las teclas debouncer.

--- ejer2-template/src/sAPI_program.c
+++ ejer2-template/src/sAPI_program.c
@@ -12,6 +12,10 @@
 
 typedef enum {LED_ON, LED_OFF} fsmBlinky_t;
 typedef enum {BUTTON_UP,BUTTON_FALLING,BUTTON_DOWN,BUTTON_RISING} fsmDebouncer_t;
+typedef struct {
+	bool_t tecPresseed = FALSE;
+	bool_t tecReleased = FALSE;
+}tecEvents_t;
 
 /*==================[definiciones de datos internos]=========================*/
 
@@ -21,14 +25,19 @@ fsmBlinky_t state = LED_ON;
 fsmDebouncer_t state_t1 = BUTTON_DOWN;
 fsmDebouncer_t state_t2 = BUTTON_DOWN;
 
+//Definicion de delays para el delay y tambien para los correspondientes debouncer
 delay_t blinkyDelay;
 delay_t tec1DelayDebouncer;
 delay_t tec2DdelayDebouncer;
+//Definicion de eventos para las correspondientes teclas.
+tecEvents_t tec1Events;
+tecEvents_t tec2Events;
 
 /*==================[Tipos LED y Tipos de tiempo]=========================*/
 #define T_1 50
 #define T_2 100
 #define T_3 1000
+#define T_Debouncer 40
 
 const uint16_t tiempoEncendido[] = {T_1,T_2,T_3};
 const gpioMap_t leds[] = {LEDR,  LEDG,  LEDB, LED1 , LED2, LED3};
@@ -40,6 +49,8 @@ const gpioMap_t leds[] = {LEDR,  LEDG,  LEDB, LED1 , LED2, LED3};
 
 /*==================[declaraciones de funciones internas]====================*/
 void fsmBlinky(void);
+void fsmTec1Debouncer (void);
+void fsmTec2Debouncer (void);
 
 /*==================[declaraciones de funciones externas]====================*/
 
@@ -47,12 +58,11 @@ void fsmBlinky(void);
 
 // FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE ENCENDIDO O RESET.
 int main( void ){
-
 	// ---------- CONFIGURACIONES ------------------------------
-
 	// Inicializar y configurar la plataforma
 	boardConfig();
-
+	uint8_t indiceTiempo = 0;
+	uint8_t indiceLed = 0;
 	// Crear varias variables del tipo booleano
 	bool_t tec1Value = OFF;
 	bool_t tec2Value = OFF;
@@ -62,11 +72,12 @@ int main( void ){
 	// ---------- REPETIR POR SIEMPRE --------------------------
 	while( TRUE )
 	{
-		fsmBlinky();
-
-
-
-
+		//fsmBlinky();
+		fsmTec1Debouncer();
+		if(tec1Events.tecPresseed == TRUE)
+			gpioWrite(leds[0],ON);
+		if(tec1Events.tecReleased == TRUE)
+			gpioWrite(leds[0],OFF);
 	}
 
 	// NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta
@@ -119,6 +130,85 @@ void fsmBlinky(void){
 
 	return;
 }
+
+void fsmTec1Debouncer (void) {
+	switch(state_t1){
+	case BUTTON_UP:
+		tec1Events.tecPresseed = FALSE;
+		tec1Events.tecReleased = FALSE;
+		//Si la tecla no esta pulsada
+		if (gpioRead(TEC1)){
+			state_t1 = BUTTON_FALLING;
+		}
+		else {
+			state_t1 = BUTTON_UP;
+		}
+		break;
+	case BUTTON_FALLING:
+		tec1Events.tecPresseed = FALSE;
+		tec1Events.tecReleased = FALSE;
+		//Si el delay no esta corriendo lo pongo a correr
+		if(!tec1DelayDebouncer.running){
+			delayConfig(&tec1DelayDebouncer,T_Debouncer);
+			//Activo el delay para que empiece a correr
+			delayRead(&tec1DelayDebouncer);
+			state_t1 = BUTTON_FALLING;
+		}
+		//Tiempo expirado
+		if(delayRead(&tec1DelayDebouncer)){
+			//Si la tecla no esta pulsada
+			if(gpioRead(TEC1)){
+				tec1Events.tecReleased = TRUE;
+				tec1Events.tecPresseed = FALSE;
+				state_t1 = BUTTON_DOWN;
+			}
+			else {
+				tec1Events.tecReleased = FALSE;
+				tec1Events.tecPresseed = FALSE;
+				state_t1 = BUTTON_UP;
+			}
+		}
+		break;
+	case BUTTON_DOWN:
+		tec1Events.tecPresseed = FALSE;
+		tec1Events.tecReleased = FALSE;
+		//Si la tecla esta pulsada
+		if (!gpioRead(TEC1)){
+			state_t1 = BUTTON_RISING;
+		}
+		else {
+			state_t1 = BUTTON_DOWN;
+		}
+		break;
+	case BUTTON_RISING:
+		tec1Events.tecPresseed = FALSE;
+		tec1Events.tecReleased = FALSE;
+		//Si el delay no esta corriendo lo pongo a correr
+		if(!tec1DelayDebouncer.running){
+			delayConfig(&tec1DelayDebouncer,T_Debouncer);
+			//Activo el delay para que empiece a correr
+			delayRead(&tec1DelayDebouncer);
+			state_t1 = BUTTON_RISING;
+		}
+		//Tiempo expirado
+		if(delayRead(&tec1DelayDebouncer)){
+			//Si la tecla  esta pulsada
+			if(!gpioRead(TEC1)){
+				tec1Events.tecReleased = FALSE;
+				tec1Events.tecPresseed = TRUE;
+				state_t1 = BUTTON_UP;
+			}
+			else {
+				tec1Events.tecReleased = FALSE;
+				tec1Events.tecPresseed = FALSE;
+				state_t1 = BUTTON_DOWN;
+			}
+		}
+		break;
+	}
+}
+
+
 /*==================[definiciones de funciones externas]=====================*/
 
 /*==================[fin del archivo]========================================*/
